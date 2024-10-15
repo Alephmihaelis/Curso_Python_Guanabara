4e70dfc29c7a4be608644f1a9646e94c147e0629
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-15 14:59:20 -0300

    Update desafio1.py

--- aula04_primeiros_comandos/desafio1.py
+++ aula04_primeiros_comandos/desafio1.py
@@ -3,3 +3,6 @@ print('Olá, mundo!')
 
 nome = input('Qual seu nome? ')
 print('Olá, ' + nome + '! É um prazer te conhecer!')
+
+nome2 = input('Qual seu nome? ')
+print('Olá, {}! É um prazer conhecer você. :D'.format(nome2))
