a78ca21e5fd5cc9dd0b064d874dd05e50789c3b0
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-15 11:20:57 -0300

    primeiros_comandos_py
    Treino da aula 4: primeiros comandos em Python3

---
+++ aula04_primeiros_comandos/desafio3.py
@@ -0,0 +1,4 @@
+
+primeiro_numero = input('Primeiro número: ')
+segundo_numero = input('Segundo número: ')
+print('A soma é', primeiro_numero + segundo_numero)
---
+++ aula04_primeiros_comandos/primeiros_comandos.py
@@ -0,0 +1,18 @@
+
+print('Olá, mundo!')
+
+print(7+4)
+
+print('7' + '4')
+
+nome = 'Alefe'
+idade = 24
+peso = 75.7
+
+print(nome, idade, peso)
+
+nome = input('Qual seu nome?' )
+idade = input('Qual sua idade?' )
+peso = input('Qual seu peso? ')
+
+print(nome, idade, peso)
