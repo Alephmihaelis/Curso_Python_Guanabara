e8e11173111714175c3243ceb4cb566f0a9bd41a
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-18 18:26:49 -0300

    Exercícios 7 e 8

--- aula07_operadores_aritmeticos/desafio7.py
+++ aula07_operadores_aritmeticos/desafio7.py
@@ -1,2 +1,7 @@
 
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+alpha = float(input('Digite a primeira nota: '))
+beta = float(input('Digite a segunda nota: '))
+
+print('As notas do aluno são {} e {}.\nA média do aluno é {:.1f}'.format(alpha, beta, (alpha + beta) / 2))
--- aula07_operadores_aritmeticos/desafio8.py
+++ aula07_operadores_aritmeticos/desafio8.py
@@ -1,2 +1,4 @@
 
 # Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+alpha = float(input('Digite um valor em metros: '))
