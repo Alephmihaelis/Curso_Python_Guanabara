550b35b9580b9edba5e5742451789edde006d663
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-18 18:09:39 -0300

    Update desafio6.py

--- aula07_operadores_aritmeticos/desafio6.py
+++ aula07_operadores_aritmeticos/desafio6.py
@@ -2,6 +2,6 @@
 # Crie um algoritmo que leia um número e mostre seu dobro, triplo e raiz quadrada.
 
 alpha = int(input('Digite um número: '))
-print('Você digitou "{}".\nO dobro de "{}" vale {}.\nO triplo de "{}" é {}.\nA raiz quadrada de "{}" vale {:.2f}.'.format(alpha, alpha, (alpha*2), alpha, (alpha*3), alpha, (alpha**(1/2))))
+print('Você digitou "{}".\nO dobro de "{}" vale {}.\nO triplo de "{}" é {}.\nA raiz quadrada de "{}" vale {:.1f}.'.format(alpha, alpha, (alpha*2), alpha, (alpha*3), alpha, (alpha**(1/2))))
 
 # Feito com uma variável só, porque o resultado é exibido uma só vez.
