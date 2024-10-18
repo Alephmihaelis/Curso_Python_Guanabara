4d387030a3056fcdb0db33889155f38959fd7c05
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-18 17:56:25 -0300

    Exercícios 5 e 6

--- aula07_operadores_aritmeticos/desafio5.py
+++ aula07_operadores_aritmeticos/desafio5.py
@@ -1,2 +1,8 @@
 
 # Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+alpha = int(input('Digite um número qualquer: '))
+
+# Como o resultado será usado para uma coisa só, não o armazenarei dentro de uma variável; em vez disso, passá-lo-ei diretamente na função format.
+
+print('O antecessor de "{}" é "{}".\nO sucessor de "{}" é "{}".'.format(alpha, (alpha - 1), alpha, (alpha + 1)))
--- aula07_operadores_aritmeticos/desafio6.py
+++ aula07_operadores_aritmeticos/desafio6.py
@@ -1,2 +1,5 @@
 
 # Crie um algoritmo que leia um número e mostre seu dobro, triplo e raiz quadrada.
+
+alpha = int(input('Digite um número: '))
+print('Você digitou "{}".\nO dobro de "{}" é {}.\nO triplo de "{}" é {}.\nA raiz quadrada de "{}" é {:.0f}.'.format(alpha, alpha, (alpha*2), alpha, (alpha*3), alpha, (alpha**(1/2))))
