11350a232ed7a104540ee5b2ca42b3639f08308f
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-18 13:01:45 -0300

    Atualização 10.18

---
+++ aula07_operadores_aritmeticos/exemplos.py
@@ -0,0 +1,43 @@
+
+# Exemplos
+
+alfa = 5+3*2
+beta = 3*5+4**2
+gama = 3*(5+4)**2
+
+print('O resultado de "a" é {}. O resultado de "b" é {}. O resultado de "c" é {}.'.format(a, b, c))
+
+delta = 5+3*2
+epsilon = 5**2
+zeta = 5**3
+eta = 19//2
+teta = 19/2
+iota = 356**522
+kappa = 18%2
+lambada = 122%3
+mi = 4**3
+
+pow(4,3)
+
+ni = 81**(1/2)
+csi = 25**(1/2)
+omicron = 127**(1/3)
+
+print('Oi' + 'Olá')
+print('Oi'*5)
+print('='*20)
+
+name = input('Qual é seu nome? ')
+print('Prazer em te conhecer, {:=>20}!'.format(name))
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print('A soma vale {}'.format(n1+n2))
+
