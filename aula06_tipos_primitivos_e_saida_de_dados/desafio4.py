900130b74369e855fa6d1056caec276d45801c65
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-17 22:36:23 -0300

    Update desafio4.py

--- aula06_tipos_primitivos_e_saida_de_dados/desafio4.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio4.py
@@ -1,1 +1,14 @@
 # Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+object = input('Digite alguma coisa: ')
+print('O tipo primitivo de "{}" é {}'.format(object, type(object)))
+print('"{}" só tem espaços? {}'.format(object, object.isspace()))
+print('"{}" é um número? {}'.format(object, object.isnumeric()))
+print('"{}" é alfabético? {}'.format(object, object.isalpha()))
+print('"{}" é alfanumérico? {}'.format(object, object.isalnum()))
+print('"{}" só tem letras maiúsculas? {}'.format(object, object.isupper()))
+print('"{}" só tem letras minúsculas? {}'.format(object, object.islower()))
+print('"{}" está capitalizado? {}'.format(object, object.istitle()))
+print('Em "{}", todos os caracteres são ASCII? {}'.format(object, object.isascii()))
+print('Em "{}", todos os caracteres são decimais? {}'.format(object, object.isdecimal()))
+print('"{}" é um identificador válido? {}'.format(object, object.isidentifier()))
