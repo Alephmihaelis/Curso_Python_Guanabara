1d71843b50e2f888ff5425c7d7a1f3ad61e7ab70
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 17:34:54 -0300

    Update desafio11.py

--- aula07_operadores_aritmeticos/desafio11.py
+++ aula07_operadores_aritmeticos/desafio11.py
@@ -1,3 +1,7 @@
 
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcule sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
 
+alpha = float(input('Digite em metros a largura da parede: '))
+beta = float(input('Digite em metros a altura da parede: '))
+
+print('Uma parede de {:.2f}x{:.2f} tem uma área de {:.3f}m². \nNecessitar-se-á de {:.2f}l de tinta para ser pintada.'.format(alpha, beta, (alpha * beta), (alpha * beta) / 2 ))
