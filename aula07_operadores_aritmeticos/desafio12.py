c184142d01873d344c7f5f44400d179e4bbdcb3e
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 17:57:29 -0300

    Update desafio12.py

--- aula07_operadores_aritmeticos/desafio12.py
+++ aula07_operadores_aritmeticos/desafio12.py
@@ -1,2 +1,5 @@
 
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+preco = float(input('Valor do produto: R$'))
+print('O produto de R${:.2f} custará, com desconto de 5%, R${:.2f}'.format(preco, preco-(preco*5/100)))
