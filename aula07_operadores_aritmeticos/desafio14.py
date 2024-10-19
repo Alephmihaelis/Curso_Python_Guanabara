be1bf990a3945df05afe50aca3510623fc1eb91f
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 18:15:20 -0300

    Resolução do desafio 14 e criação do 15

--- aula07_operadores_aritmeticos/desafio14.py
+++ aula07_operadores_aritmeticos/desafio14.py
@@ -1,5 +1,13 @@
 
 # Converta uma temperatura digitada em Celsius para Fahrenheit.
+# OBS.: Adicionei também o código que faz a conversão de Fahrenheit para Celsius.
 
+print('Temperatura de Celsius para Fahrenheit')
 temp_celsius = float(input('Temperatura em ºC: '))
-print('A temperatura {}ºC, em Fahrenheit, vale {}ºF'.format(temp_celsius, temp_celsius * 1.8 + 32))
+print('A temperatura {:.1f}ºC, em Fahrenheit, vale {:.1f}ºF'.format(temp_celsius, temp_celsius * 1.8 + 32))
+
+print('-'*20)
+
+print('Temperatura de Fahrenheit para Celsius')
+temp_fahrenheit = float(input('Temperatura em ºF: '))
+print('A temperatura {:.1f}ºF equivale a {:.1f}ºC'.format(temp_fahrenheit, (temp_fahrenheit - 32) / 1.8))
