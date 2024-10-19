145e6275fe3fe66451d8830536606e7220389569
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 18:06:06 -0300

    Atualização com os exercícios 14 e 15
    Os exercícios 14 e 15 ainda precisam ser resolvidos.

--- aula07_operadores_aritmeticos/desafio13.py
+++ aula07_operadores_aritmeticos/desafio13.py
@@ -1,2 +1,8 @@
 
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input('Salário do funcionário: R$'))
+
+print('Com 15% de aumento, o salário do funcionário, que é de R${:.2f}, passará a ser \nR${:.2f}'.format(salario, salario + (salario * 15/100)))
+
+# Continuo evitando o uso desnecessário de variáveis, uma vez que o resultado é exibido uma vez só.
