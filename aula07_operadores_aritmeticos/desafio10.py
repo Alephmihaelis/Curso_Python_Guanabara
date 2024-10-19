a46fac370da3e0baf8856571ba4e550ffead0544
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 17:23:35 -0300

    Exercícios 9 e 10

--- aula07_operadores_aritmeticos/desafio10.py
+++ aula07_operadores_aritmeticos/desafio10.py
@@ -1,2 +1,7 @@
 
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere US$1.00 = R$3.27
+
+# OBS.: Em vez de R$3.27, pus a real cotação do dólar, que hoje é R$5.69
+
+alpha = float(input('Quanto dinheiro você tem? R$'))
+print('Com R${:.2f}, você pode comprar US${:.2f}'.format(alpha, (alpha / 5.69)))
--- aula07_operadores_aritmeticos/desafio9.py
+++ aula07_operadores_aritmeticos/desafio9.py
@@ -1,2 +1,10 @@
 
 # Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+alpha = int(input('Digite um número para ver-lhe a tabuada: '))
+
+print('-'*25)
+
+print('{} x 1 = {}\n{} x 2 = {}\n{} x 3 = {}\n{} x 4 = {}\n{} x 5 = {}\n{} x 6 = {}\n{} x 7 = {}\n{} x 8 = {}\n{} x 9 = {}\n{} x 10 = {}'.format(alpha, (alpha*1), alpha, (alpha*2), alpha, (alpha*3), alpha, (alpha*4), alpha, (alpha*5), alpha, (alpha*6), alpha, (alpha*7), alpha, (alpha*8), alpha, (alpha*9), alpha, (alpha*10)))
+
+print('-'*25)
