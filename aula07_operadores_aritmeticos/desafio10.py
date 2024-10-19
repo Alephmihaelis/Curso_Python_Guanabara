948f6c46e6fff7b0a6c267a06ce4715d83882514
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 17:27:38 -0300

    Update desafio10.py

--- aula07_operadores_aritmeticos/desafio10.py
+++ aula07_operadores_aritmeticos/desafio10.py
@@ -2,6 +2,7 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere US$1.00 = R$3.27
 
 # OBS.: Em vez de R$3.27, pus a real cotação do dólar, que hoje é R$5.69
+# OBS².: Adicionei euro e iene.
 
 alpha = float(input('Quanto dinheiro você tem? R$'))
-print('Com R${:.2f}, você pode comprar US${:.2f}'.format(alpha, (alpha / 5.69)))
+print('Com R${:.2f}, você pode comprar:\nUS${:.2f}\n€{:.2f}\n¥{:.2f}'.format(alpha, (alpha / 5.69), (alpha / 6.19), (alpha / 0.038)))
