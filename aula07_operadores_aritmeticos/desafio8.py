57410ab203467d83e83c092409a2fe6da7ab38a1
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-19 16:47:57 -0300

    Update desafio8.py

--- aula07_operadores_aritmeticos/desafio8.py
+++ aula07_operadores_aritmeticos/desafio8.py
@@ -1,4 +1,7 @@
 
-# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+# Escreva um programa que leia um valor em metros e o exiba convertido em outras unidades de comprimento.
 
 alpha = float(input('Digite um valor em metros: '))
+print('Analisando o valor informado, percebe-se que:\n{}m equivale(m) a {}km\n{}m equivale(m) a {}hm\n{}m equivale(m) a {}dam\n{}m equivale(m) a {:.0f}dm\n{}m equivale(m) a {:.0f}cm\n{}m equivale(m) a {:.0f}mm'.format(alpha, (alpha/1000), alpha, (alpha/100), alpha, (alpha/10), alpha, (alpha*10), alpha, (alpha*100), alpha, (alpha*1000)))
+
+# Ao depois procurarei maneiras de tornar este código mais legível.
