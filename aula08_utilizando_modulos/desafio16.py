33a098b5c10ffd7406e7fc846f1b718befe36d90
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 13:03:42 -0300

    Update desafio16.py

--- aula08_utilizando_modulos/desafio16.py
+++ aula08_utilizando_modulos/desafio16.py
@@ -1,3 +1,17 @@
 
+from math import trunc, floor
+
 # Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira. Ex: Digite um número: 6.127 :: O número 6.127 tem a parte inteira 6.
 
+num_real = float(input('Digite um número real: '))
+
+# Há dois modos de resolver este exercício. Posso resolvê-lo ou com a função `int`, ou com o método `trunc`, ou com o método `floor`.
+
+# Resolução com a função `int`
+print('A porção inteira do número {} é {}'.format(num_real, int(num_real)))
+
+# Resolução com o método `trunc`
+print('A porção inteira do número {} é {}'.format(num_real, trunc(num_real)))
+
+# Resolução com o método `floor`
+print('A porção inteira do número {} é {}'.format(num_real, floor(num_real)))
