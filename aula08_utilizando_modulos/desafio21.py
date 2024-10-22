bdfc84252e785f5ae21ce0e674d97f2b42cb5cf2
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 14:56:33 -0300

    Resolução do exercício 21

--- aula08_utilizando_modulos/desafio21.py
+++ aula08_utilizando_modulos/desafio21.py
@@ -1,2 +1,11 @@
 
 # Faça um programa que abra e reproduza o áudio de um arquivo MP3.
+
+from playsound3 import playsound
+
+playsound('/home/ivstitiam/Área de Trabalho/Programação/curso_python_guanabara/Curso_Python_Guanabara/aula08_utilizando_modulos/audio.mp3')
+
+'''
+Observações: 1. por alguma razão, não consegui reproduzir o áudio com a biblioteca PyGame.
+             2. por alguma razão, o áudio só toca com se informamos o caminho absoluto do arquivo como argumento.
+'''
