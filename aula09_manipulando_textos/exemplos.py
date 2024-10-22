79dd354ddb1316f1681b314e9e426ba893f548b3
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 15:52:21 -0300

    Pequenas alterações

--- aula09_manipulando_textos/exemplos.py
+++ aula09_manipulando_textos/exemplos.py
@@ -2,3 +2,9 @@
 # Manipulação de cadeias de texto.
 
 frase = 'Estou aprendendo Python'
+print(frase[9])
+print(frase[9:13])
+print(frase[9:21:2])
+print(frase[:5])
+
+# Os exemplos acima são de `fatiamento`
