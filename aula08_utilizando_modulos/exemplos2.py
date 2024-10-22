f882c1900b43bc3aa8ec87a556db770f6538c68d
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 12:41:33 -0300

    Create exemplos2.py

---
+++ aula08_utilizando_modulos/exemplos2.py
@@ -0,0 +1,9 @@
+import emoji
+# import random
+
+# num = random.randint(1, 10)
+# print(num)
+
+print(emoji.emojize('Olá, mundo! :globe_showing_Americas:'))
+
+# O parâmetro `use_aliases` não é mais necessário.
