5c0a6eb0101e137e8b36201491f55a8318c188de
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 12:25:27 -0300

    Create exemplos.py

---
+++ aula08_utilizando_modulos/exemplos.py
@@ -0,0 +1,7 @@
+
+# import math → importa todas as funcionalidades de `math`
+
+from math import sqrt, ceil, floor # → importa somente as funcionalidades `sqrt` e `ceil` de `math`
+
+num = int(input('Número: '))
+print('A raiz de {} vale {:.2f}'.format(num, sqrt(num))) # → Mais uma vez, estou evitando o uso de variáveis desnecessárias
