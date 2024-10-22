dd920fe6d26b6e90fa0587e30c62a9eb29eb0e80
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 14:02:31 -0300

    Update desafio19.py

--- aula08_utilizando_modulos/desafio19.py
+++ aula08_utilizando_modulos/desafio19.py
@@ -1,2 +1,22 @@
 
 # Um professor quer sortear um de seus quatro alunos para apagar o quadro. Faça um programa que o ajude, lendo o nome deles e escrevendo o nome do escolhido.
+
+from random import choice
+
+# Resolução em que pensei.
+
+alunos = 'Álefe', 'Gisele', 'Pedro', 'João'
+print('O aluno escolhido foi {}!'.format(choice(alunos)))
+
+# O professor nomeou os alnos com um `input`
+
+aluno_um = input('Primeiro aluno: ')
+aluno_dois = input('Segundo aluno: ')
+aluno_tres = input('Terceiro aluno: ')
+aluno_quatro = input('Quatro alunos: ')
+
+# O professor cria uma `lista`. Listas são representadas por `[]`.
+
+# Criação da lista
+lista_alunos = [aluno_um, aluno_dois, aluno_tres, aluno_quatro]
+print('O aluno escolhido foi {}!'.format(choice(lista_alunos)))
