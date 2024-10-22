ac5d7d95f1a5f4134b09c127e9995b85d3cec3e8
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-22 14:20:15 -0300

    Update desafio20.py

--- aula08_utilizando_modulos/desafio20.py
+++ aula08_utilizando_modulos/desafio20.py
@@ -1,2 +1,13 @@
 
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalho dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import sample
+
+aluno_um = input('Primeiro aluno: ')
+aluno_dois = input('Segundo aluno: ')
+aluno_tres = input('Terceiro aluno: ')
+aluno_quatro = input('Quarto aluno: ')
+
+alunos = [aluno_um, aluno_dois, aluno_tres, aluno_quatro]
+
+print('A ordem de apresentação será:\n{}'.format(sample(alunos, k=4)))
