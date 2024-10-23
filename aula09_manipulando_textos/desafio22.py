2d6814b2bafaa361ad5f21b63d0e49885aa221e6
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-23 00:01:23 -0300

    Update desafio22.py

--- aula09_manipulando_textos/desafio22.py
+++ aula09_manipulando_textos/desafio22.py
@@ -6,3 +6,13 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 3. Quantas letras ao todo (sem considerar espaços);
 4. Quantas letras tem o primeiro nome.
 '''
+
+full_name = input('Nome completo:')
+
+print('Nome com todas as letras maiúsculas: {}\nNome com todas as letras minúsculas: {}\nLetras ao todo, sem considerar espaços: {}'.format(
+    full_name.upper(),
+    full_name.lower(),
+    len(full_name.replace(" ", ""))
+    ))
+
+# Por alguma razão, este código NÃO ESTÁ FUNCIONANDO. Já testei tudo. A lógica me parece correta, e também a sintaxe. Não sei qual possa ser o erro.
