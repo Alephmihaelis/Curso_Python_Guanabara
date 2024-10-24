1eb041cc7fb8abd8274d6b5b9ce3ca6ee63a7ce0
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-24 11:53:33 -0300

    Resolução dos desafios 25 e 26

--- aula09_manipulando_textos/desafio25.py
+++ aula09_manipulando_textos/desafio25.py
@@ -2,3 +2,9 @@
 '''
 Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome
 '''
+
+name = input('Digite seu nome: ').strip().title()
+print('Olá, {}! \nVocê tem "Silva" no nome? {}'.format(
+    name,
+    'Silva' in name.split()
+))
--- aula09_manipulando_textos/desafio26.py
+++ aula09_manipulando_textos/desafio26.py
@@ -5,3 +5,10 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 2. Em que posição ela aparece a primeira vez;
 3. Em que posição ela aparece a última vez.
 '''
+
+frase = input('Frase: ').strip().upper()
+print('Quantas vezes a letra "A" aparece na frase? {}\nEm que posição "A" aparece a primeira vez? {}\nEm que posição "A" aparece a última vez? {}'.format(
+    frase.count('A'),
+    frase.replace(" ", "").find('A') + 1, # O comando `replace` e o `+1` deixam o código mais compreensível para o usuário final. Isso facilita a leitura do usuário, pois ele não considerará o espaço do meio como uma letra, ao contrário do Python.
+    frase.replace(" ", "").rfind('A') + 1
+))
