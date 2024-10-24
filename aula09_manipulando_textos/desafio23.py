bd59272bbb6f0c429541cc1f8ba9d0d2c23dc28d
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-24 11:17:40 -0300

    Update desafio23.py

--- aula09_manipulando_textos/desafio23.py
+++ aula09_manipulando_textos/desafio23.py
@@ -7,4 +7,25 @@ Unidade: 4
 Dezena: 3
 Centena: 8
 Milhar: 1
+
+
+num = input('Número entre 0 e 9999: ').strip()
+print('MILHAR: {} \nCENTENA: {} \nDEZENA: {} \nUNIDADE: {}'.format(
+    num[0],
+    num[1],
+    num[2],
+    num[3]
+))
+
+# Making this code this way will generate an error if `num` does not contain all four digits.
 '''
+
+# Teacher's code:
+
+n = int(input('Número entre 0 e 9999: '))
+print('UNIDADE: {}\nDEZENA: {}\nCENTENA: {}\nMILHAR: {}'.format(
+    n // 1 % 10,
+    n // 10 % 10,
+    n // 100 % 10,
+    n // 1000 % 10
+))
