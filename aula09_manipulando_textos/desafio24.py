3d49170cf3b0ccc53d89ab60bcd9b9c1ebcb87e2
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-24 11:33:00 -0300

    Update desafio24.py

--- aula09_manipulando_textos/desafio24.py
+++ aula09_manipulando_textos/desafio24.py
@@ -1,4 +1,12 @@
 
 '''
-Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavr 'Santo'
+Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavra 'Santo'
 '''
+
+city_name = input('Nome da cidade: ').strip().title()
+print('A cidade digitada foi "{}".\nEla começa com "Santo"? {}'.format(
+    city_name,
+    'Santo' in city_name.split()[0]
+))
+
+# Meu código foi um pouco diferente do do professor. Usei o operador `in`.
