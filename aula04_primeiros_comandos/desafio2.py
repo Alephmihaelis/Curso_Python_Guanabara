3bf12173b04f61282dbabca859a2bd23d733d60b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 11:34:15 -0300

    Adição de cores ao exercício

--- aula04_primeiros_comandos/desafio2.py
+++ aula04_primeiros_comandos/desafio2.py
@@ -1,5 +1,5 @@
 
-dia = input('Em que dia você nasceu? ')
-mes = input('Em que mês você nasceu? ')
-ano = input('Em que ano você nasceu? ')
+dia = input('\033[1;32;43mEm que dia você nasceu?\033[m ')
+mes = input('\033[1;33;44mEm que mês você nasceu?\033[m ')
+ano = input('\033[1;34;45mEm que ano você nasceu?\033[m ')
 print('Você nasceu no dia', dia, 'de', mes, 'de', ano + '. Correto?')
