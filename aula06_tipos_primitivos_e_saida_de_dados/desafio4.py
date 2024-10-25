76883a4dc3616b4f5ac57a849e0ed733cdb9701f
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 11:52:06 -0300

    Melhorias na formatação e adição de cores
    Melhorias na formatação de `.format`

--- aula06_tipos_primitivos_e_saida_de_dados/desafio4.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio4.py
@@ -1,14 +1,58 @@
+
 # Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
 
 object = input('Digite alguma coisa: ')
-print('O tipo primitivo de "{}" é {}'.format(object, type(object)))
-print('"{}" só tem espaços? {}'.format(object, object.isspace()))
-print('"{}" pode ser um número? {}'.format(object, object.isnumeric()))
-print('"{}" é alfabético? {}'.format(object, object.isalpha()))
-print('"{}" é alfanumérico? {}'.format(object, object.isalnum()))
-print('"{}" só tem letras maiúsculas? {}'.format(object, object.isupper()))
-print('"{}" só tem letras minúsculas? {}'.format(object, object.islower()))
-print('"{}" está capitalizado? {}'.format(object, object.istitle()))
-print('Em "{}", todos os caracteres são ASCII? {}'.format(object, object.isascii()))
-print('Em "{}", todos os caracteres são decimais? {}'.format(object, object.isdecimal()))
-print('"{}" é um identificador válido? {}'.format(object, object.isidentifier()))
+print('\033[7;36;43mO tipo primitivo de "{}" é {}\033[m'.format(
+    object,
+    type(object)
+    ))
+
+print('\033[1;32;43m"{}" só tem espaços? {}\033[m'.format(
+    object,
+    object.isspace()
+    ))
+
+print('\033[4;33;44m"{}" pode ser um número? {}\033[m'.format(
+    object,
+    object.isnumeric()
+    ))
+
+print('\033[1;34;45m"{}" é alfabético? {}\033[m'.format(
+    object,
+    object.isalpha()
+    ))
+
+print('\033[32;47m"{}" é alfanumérico? {}\033[m'.format(
+    object,
+    object.isalnum()
+    ))
+
+print('\033[36;46m"{}" só tem letras maiúsculas? {}\033[m'.format(
+    object,
+    object.isupper()
+    ))
+
+print('\033[1;31;41m"{}" só tem letras minúsculas? {}\033[m'.format(
+    object,
+    object.islower()
+    ))
+
+print('\033[4;32;43m"{}" está capitalizado? {}\033[m'.format(
+    object,
+    object.istitle()
+    ))
+
+print('\033[1;33;44mEm "{}", todos os caracteres são ASCII? {}\033[m'.format(
+    object,
+    object.isascii()
+    ))
+
+print('\033[7;31;45mEm "{}", todos os caracteres são decimais? {}\033[m'.format(
+    object,
+    object.isdecimal()
+    ))
+
+print('\033[1;34;44m"{}" é um identificador válido? {}\033[m'.format(
+    object,
+    object.isidentifier()
+    ))
