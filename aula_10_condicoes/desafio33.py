b225befaacc6bc49582f7afdcfa6999bfa2881e3
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 09:35:23 -0300

    Resolução do desafio 33

--- aula_10_condicoes/desafio33.py
+++ aula_10_condicoes/desafio33.py
@@ -2,3 +2,29 @@
 '''
 Faça um programa que leia três números e mostre qual o maior e qual é o menor.
 '''
+
+from time import sleep
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+num3 = int(input('Terceiro número: '))
+
+maior = num1
+
+print('Dos três números ditos, o maior e o menor são...')
+sleep(1)
+
+if num2 > maior and num2 > num3:
+    maior = num2
+if num3 > maior and num3 > num2:
+    maior = num3
+
+print('O maior número é: {}'.format(maior))
+
+menor = num2
+if num1 < menor and num1 < num3:
+    menor = num1
+if num3 < menor and num3 < num1:
+    menor = num3
+
+print('O menor número é: {}'.format(menor))
