1033ec8e1f2e22e1d0617897673fddda3903574d
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 08:50:34 -0300

    Update desafio30.py

--- aula_10_condicoes/desafio30.py
+++ aula_10_condicoes/desafio30.py
@@ -2,3 +2,16 @@
 '''
 Crie um programa que leia um número inteiro qualquer e mostre na tela se ele é par ou ímpar.
 '''
+
+from time import sleep
+
+num = int(input('Digite um número para saber se ele é par ou ímpar: '))
+
+print('Analisando...')
+
+sleep(1.3)
+
+if num % 2 == 0:
+    print('O número {} é PAR.'.format(num))
+else:
+    print('O número {} é ÍMPAR.'.format(num))
