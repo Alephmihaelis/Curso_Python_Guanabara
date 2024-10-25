16ed08af4fac4467980ecfc5d08682f6f0349afa
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 08:43:54 -0300

    Resolução dos exercícios 28 e 29

--- aula_10_condicoes/desafio28.py
+++ aula_10_condicoes/desafio28.py
@@ -1,4 +1,21 @@
 
 '''
-Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para que o usuário tente descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
+Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para que o usuário tente descobrir  o número escolhido pelo computador.
+O programa deverá escrever na tela se o usuário venceu ou perdeu.
 '''
+
+import emoji
+from random import randint # Importei somente a função `randint`, em vez de toda a biblioteca `random`. Isto economiza memória.
+from time import sleep
+
+print('Pensarei em um número entre 0 e 5. Tente adivinhá-lo!')
+user_num = int(input('Número: '))
+pc_num = randint(0, 5)
+
+print('Processando...')
+sleep(2)
+
+if user_num == pc_num:
+    print(emoji.emojize('PARABÉNS! Você acertou! :grinning_face_with_smiling_eyes:'))
+else:
+    print(emoji.emojize('Você errou! :sad_but_relieved_face:'))
--- aula_10_condicoes/desafio29.py
+++ aula_10_condicoes/desafio29.py
@@ -4,3 +4,19 @@ Escreva um programa que leia a velocidade de um carro.
 Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.
 A multa custará R$7.00 por cada km acima do limite.
 '''
+
+import emoji
+from time import sleep
+
+speed = float(input('Velocidade do carro: '))
+
+print('Analisando...')
+sleep(2)
+
+if speed > 80:
+    print(emoji.emojize('VOCÊ FOI MULTADO! :angry_face:\nCada quilômetro a mais te custará R$7.00.\nUma vez que ultrapassaste 80km/h, sua multa custará R${:.2f}'.format(
+        (speed - 80) * 7
+    )))
+else:
+    print(emoji.emojize('Você está dentro da velocidade permitida! :smiling_face_with_open_hands:'))
+print('Tenha uma boa viagem!')
