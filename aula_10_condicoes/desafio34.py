0a5328ba42427273702655ec32a2766ab9c20581
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 10:23:50 -0300

    Resolução dos exercícios 34 e 35

--- aula_10_condicoes/desafio34.py
+++ aula_10_condicoes/desafio34.py
@@ -4,3 +4,18 @@ Escreva um programa que pergunte o salário de um funcionário e calcule o valor
 Para salários superiores a R$1.250.00, calcule um aumento de 10%.
 Para salários inferiores ou iguais a isso, calcule um aumento de 15%.
 '''
+
+salary = float(input('Salário do funcionário: R$'))
+
+if salary > 1250:
+    aumento = salary*(10/100)
+
+else:
+    aumento = salary*(15/100)
+
+new_salary = salary+aumento
+
+print('O salário do funcionário aumentou R${:.2f}\nO novo salário do funcionário será R${:.2f}'.format(
+    aumento,
+    new_salary
+    ))
--- aula_10_condicoes/desafio35.py
+++ aula_10_condicoes/desafio35.py
@@ -2,3 +2,12 @@
 '''
 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se ele pode formar um triângulo ou não.
 '''
+
+seg_um = float(input('Primeiro segmento: '))
+seg_dois = float(input('Segundo segmento: '))
+seg_tres = float(input('Terceiro segmento: '))
+
+if seg_um + seg_dois > seg_tres and seg_um + seg_tres > seg_dois and seg_dois + seg_tres > seg_um:
+    print('As medidas informadas podem formar um triângulo!')
+else:
+    print('As medidas informadas não podem formar um triângulo.')
