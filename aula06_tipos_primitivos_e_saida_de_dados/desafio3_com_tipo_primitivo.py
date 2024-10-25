b17808657649e4d3afb42cc1762a4601f321c492
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 11:45:46 -0300

    Melhorias na formatação de `.format`

--- aula06_tipos_primitivos_e_saida_de_dados/desafio3_com_tipo_primitivo.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio3_com_tipo_primitivo.py
@@ -2,4 +2,8 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = n1 + n2
-print('\033[7;32;41mA soma entre {} e {} vale {}\033[m'.format(n1, n2, s))
+print('\033[7;32;41mA soma entre {} e {} vale {}\033[m'.format(
+    n1,
+    n2,
+    s
+    ))
