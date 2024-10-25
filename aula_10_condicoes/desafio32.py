9b8067b73f6423239fd4104f3ab0df3219e197c4
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 09:22:52 -0300

    Update desafio32.py

--- aula_10_condicoes/desafio32.py
+++ aula_10_condicoes/desafio32.py
@@ -2,3 +2,24 @@
 '''
 Faça um programa que leia um ano qualquer na tela e mostre se ele é bissexto.
 '''
+
+from datetime import date
+from time import sleep
+
+print('Vamos analisar se um ano é bissexto.')
+year = int(input('Digite um ano, ou coloque 0 para analisar o ano atual: '))
+
+print('Analisando...')
+sleep(1.0)
+
+if year == 0:
+    year = date.today().year
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(
+        year
+        ))
+else:
+    print('O ano {} não é BISSEXTO.'.format(
+        year
+        ))
