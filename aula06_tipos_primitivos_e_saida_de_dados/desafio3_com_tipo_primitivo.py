7df1aebcd159d34b81f4c355c4a6c7909563ac12
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 11:45:17 -0300

    Adição de cores e melhorias na formatação
    Melhorias na formatação de `.format`

--- aula06_tipos_primitivos_e_saida_de_dados/desafio3_com_tipo_primitivo.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio3_com_tipo_primitivo.py
@@ -2,4 +2,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = n1 + n2
-print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+print('\033[7;32;41mA soma entre {} e {} vale {}\033[m'.format(n1, n2, s))
--- aula06_tipos_primitivos_e_saida_de_dados/desafio4_revisao.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio4_revisao.py
@@ -1,14 +1,52 @@
 
 something = input('Digite alguma coisa: ')
 
-print('O tipo primitivo de "{}" é {}'.format(something, type(something)))
-print('"{}" é alfabético? {}'.format(something, something.isalpha()))
-print('"{}" é alfanumérico? {}'.format(something, something.isalnum()))
-print('"{}" pode ser um número? {}'.format(something, something.isnumeric()))
-print('"{}" é um número? {}'.format(something, something.isdigit()))
-print('"{}" está capitalizado? {}'.format(something, something.istitle()))
-print('"{}" está de todo em letras maiúsculas? {}'.format(something, something.isupper()))
-print('"{}" está de todo em letras minúsculas? {}'.format(something, something.islower()))
-print('"{}" pode ser impresso? {}'.format(something, something.isprintable()))
-print('"{}" compõe-se só de espaços? {}'.format(something, something.isspace()))
+print('\033[4mO tipo primitivo de "{}" é {}\033[m'.format(
+    something,
+    type(something)
+    ))
 
+print('\033[1m"{}" é alfabético? {}\033[m'.format(
+    something,
+    something.isalpha()
+    ))
+
+print('\033[0;37;47m"{}" é alfanumérico? {}\033[m'.format(
+    something,
+    something.isalnum()
+    ))
+
+print('\033[7;36;36m"{}" pode ser um número? {}\033[m'.format(
+    something,
+    something.isnumeric()
+    ))
+
+print('\033[1;35;45m"{}" é um número? {}\033[m'.format(
+    something,
+    something.isdigit()
+    ))
+
+print('\033[1;34;44m"{}" está capitalizado? {}\033[m'.format(
+    something,
+    something.istitle()
+    ))
+
+print('\033[4;33;43m"{}" está de todo em letras maiúsculas? {}\033[m'.format(
+    something,
+    something.isupper()
+    ))
+
+print('\033[1;32;42m"{}" está de todo em letras minúsculas? {}\033[m'.format(
+    something,
+    something.islower()
+    ))
+
+print('"{}" pode ser impresso? {}'.format(
+    something,
+    something.isprintable()
+    ))
+
+print('"{}" compõe-se só de espaços? {}'.format(
+    something,
+    something.isspace()
+    ))
