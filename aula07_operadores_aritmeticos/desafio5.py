99e1ecec6c6310e3cd3f65ce80885abf7492885c
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 11:53:55 -0300

    Cores e formatação
    Melhorias na formatação de `.format` e adição de cores

--- aula07_operadores_aritmeticos/desafio5.py
+++ aula07_operadores_aritmeticos/desafio5.py
@@ -5,4 +5,9 @@ alpha = int(input('Digite um número qualquer: '))
 
 # Como o resultado será usado para uma coisa só, não o armazenarei dentro de uma variável; em vez disso, passá-lo-ei diretamente na função format.
 
-print('O antecessor de "{}" é "{}".\nO sucessor de "{}" é "{}".'.format(alpha, (alpha - 1), alpha, (alpha + 1)))
+print('O antecessor de \033[1m"{}\033[m" é \033[1m"{}"\033[m.\nO sucessor de \033[1m"{}"\033[m é \033[1m"{}"\033[m.'.format(
+    alpha,
+    (alpha - 1),
+    alpha,
+    (alpha + 1)
+    ))
