1df66ffc6e062bfbfc86213ce2b48f1a7ecbdcf2
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-25 11:03:47 -0300

    Create exemplos.py

---
+++ aula11_cores_no_terminal/exemplos.py
@@ -0,0 +1,7 @@
+
+print('\033[0;30;41mExemplo 1\033[m')
+print('\033[4;33;44mExemplo 2\033[m')
+print('\033[1;35;43mExemplo 3\033[m')
+print('\033[0;30;42mExemplo 4\033[m')
+print('\033[mExemplo 4\033')
+print('\033[7;30mExemplo 5\033[m')
