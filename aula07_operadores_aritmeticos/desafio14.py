efdb305e940ac1d986abc8aee1179d64480b7835
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-26 16:26:05 -0300

    Cores e formatação

--- aula07_operadores_aritmeticos/desafio14.py
+++ aula07_operadores_aritmeticos/desafio14.py
@@ -2,12 +2,28 @@
 # Converta uma temperatura digitada em Celsius para Fahrenheit.
 # OBS.: Adicionei também o código que faz a conversão de Fahrenheit para Celsius.
 
+colors = {
+    'red_bg': '\033[41m',
+    'blue_bg': '\033[44m',
+    'bold': '\033[1m',
+    'limpa': '\033[m'
+}
+
 print('Temperatura de Celsius para Fahrenheit')
 temp_c = float(input('Temperatura em ºC: '))
-print('A temperatura {:.1f}ºC, em Fahrenheit, vale {:.1f}ºF'.format(temp_c, temp_c * 1.8 + 32))
+print('{}A temperatura {:.1f}ºC, em Fahrenheit, vale {:.1f}ºF{}'.format(
+    colors['red_bg'],
+    temp_c, temp_c * 1.8 + 32,
+    colors['limpa']
+    ))
 
 print('-'*40)
 
 print('Temperatura de Fahrenheit para Celsius')
 temp_f = float(input('Temperatura em ºF: '))
-print('A temperatura {:.1f}ºF equivale a {:.1f}ºC'.format(temp_f, (temp_f - 32) / 1.8))
+print('{}A temperatura {:.1f}ºF equivale a {:.1f}ºC{}'.format(
+    colors['blue_bg'],
+    temp_f,
+    (temp_f - 32) / 1.8,
+    colors['limpa']
+    ))
--- aula07_operadores_aritmeticos/desafio15.py
+++ aula07_operadores_aritmeticos/desafio15.py
@@ -1,6 +1,17 @@
 
 # Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por km rodado.
 
+colors = {
+    'bg_mg': '\033[46m',
+    'limpa': '\033[m'
+}
+
 km_p = float(input('Quilômetros rodados: '))
 dias = int(input('Por quantos dias o carro foi alugado? '))
-print('O carro rodou {:.2f}km.\nO carro foi alugado por {} dias.\nPreço total: R${:.2f}'.format(km_p, dias, dias * 60 + 0.15 * km_p))
+print('{}O carro rodou {:.2f}km.\nO carro foi alugado por {} dias.\nPreço total: R${:.2f}{}'.format(
+    colors['bg_mg'],
+    km_p,
+    dias,
+    dias * 60 + 0.15 * km_p,
+    colors['limpa']
+    ))
