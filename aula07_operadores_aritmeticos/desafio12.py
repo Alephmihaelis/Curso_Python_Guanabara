7a47bb6a056db96d6553fc5fbc5cd14c6cbff1b4
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-26 16:18:09 -0300

    Cores e formatação

--- aula07_operadores_aritmeticos/desafio12.py
+++ aula07_operadores_aritmeticos/desafio12.py
@@ -1,5 +1,16 @@
 
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 
+colors = {
+    'bold': '\033[1m',
+    'yellow_bg': '\033[43m',
+    'limpa': '\033[m'
+}
+
 preco = float(input('Valor do produto: R$'))
-print('O produto de R${:.2f} custará, com desconto de 5%, R${:.2f}'.format(preco, preco-(preco*5/100)))
+print('{}{}O produto de R${:.2f} custará, com desconto de 5%, R${:.2f}{}'.format(
+    colors['bold'],
+    colors['yellow_bg'],
+    preco, preco-(preco*5/100),
+    colors['limpa']
+    ))
--- aula07_operadores_aritmeticos/desafio13.py
+++ aula07_operadores_aritmeticos/desafio13.py
@@ -1,8 +1,21 @@
 
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 
+colors = {
+    'green': '\033[32m',
+    'under': '\033[4m',
+    'limpa': '\033[m'
+}
+
 salario = float(input('Salário do funcionário: R$'))
 
-print('Com 15% de aumento, o salário do funcionário, que é de R${:.2f}, passará a ser \nR${:.2f}'.format(salario, salario + (salario * 15/100)))
+print('{}{}Com 15% de aumento, o salário do funcionário, que é de R${:.2f}, passará a ser \nR${:.2f}{}\n"AAAAAAAAAAAAAAAA"'.format(
+    colors['green'],
+    colors['under'],
+    salario,
+    salario + (salario * 15/100),
+    colors['limpa']
+    ))
 
 # Continuo evitando o uso desnecessário de variáveis, uma vez que o resultado é exibido uma vez só.
+# Este `"AAAAAAAAAAAAAAAA"` foi um teste; aparentemente, basta fechar o comando uma só vez.
