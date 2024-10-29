a9e87ad1e2dda1272750d8d576e288e46d99152b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-29 14:19:00 -0300

    Resolução do desafio 38

--- aula12_condicoes_aninhadas/desafio38.py
+++ aula12_condicoes_aninhadas/desafio38.py
@@ -1,2 +1,28 @@
 
-# Escreva um programa que leia dois números inteiros e os compare, mostrando na tela uma das seguintes mensagens: O primeiro valor é maior; o segundo valor é maior, não existe valor maior, os dois são iguais.
+# Escreva um programa que leia dois números inteiros e os compare, mostrando na tela uma das seguintes mensagens: O primeiro valor é maior; o segundo valor é maior; não existe valor maior: os dois são iguais.
+
+text = {
+    'bold': '\033[1m',
+    'limpa': '\033[m'
+}
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+
+if num1 > num2:
+    print('O primeiro valor é {}maior{} que o segundo'.format(
+        text['bold'],
+        text['limpa']
+    ))
+
+elif num2 > num1:
+    print('O segundo valor é {}maior{} que o primeiro'.format(
+        text['bold'],
+        text['limpa']
+    ))
+
+else:
+    print('Ambos os valores são {}iguais{}'.format(
+        text['bold'],
+        text['limpa']
+    ))
