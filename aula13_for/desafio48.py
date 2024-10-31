0fc5edff016ff8b34b46def58c370b59a7b4ad61
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-10-31 16:16:37 -0300

    Resolução dos exercícios 47 e 48

--- aula13_for/desafio48.py
+++ aula13_for/desafio48.py
@@ -1,2 +1,13 @@
 
 # Faça um programa que calcule a soma entre todos números ímpares que são múltiplos de 3 e que se encontram entre 1 e 500.
+
+soma = 0
+contador = 0
+
+for num in range(1, 501, 2):
+    if num % 3 == 0:
+        contador = contador + 1
+        soma = soma + num
+
+print('A soma de todos os valores vale {}.\nSomaram-se {} números.'.format(soma,
+                                                                           contador))
