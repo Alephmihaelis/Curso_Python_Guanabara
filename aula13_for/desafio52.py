28b2822759052d201e1f10e5a845b973f36cb117
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-02 10:41:51 -0300

    Resolução do execício 52

--- aula13_for/desafio52.py
+++ aula13_for/desafio52.py
@@ -1,2 +1,22 @@
 
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+num = int(input('Digite um número inteiro: '))
+
+tot = 0 # Contador
+
+for x in range(1, num+1):
+    if num % x == 0:
+            tot = tot + 1
+            print('\033[31m', end='')
+    else:
+          print('\033[33m', end='')
+    print(x, end=' ')
+
+print('\n\033[mO número {} foi divisível {} vezes.'.format(num, tot))
+
+if tot == 2:
+      print('Portanto, ele É primo!')
+
+else:
+      print('Portanto, ele NÃO É primo!')
