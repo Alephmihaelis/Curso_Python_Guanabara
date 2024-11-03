aee8f8a76b4fdac0b70c3fd59a35af2d42314396
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-03 07:25:40 -0300

    Resolução da revisão do desafio 47

---
+++ aula13_for/revisao_exercicios/desafio47.py
@@ -0,0 +1,9 @@
+
+# `Loop` que percorre o intervalo de 1 a 51:
+for par in range(1, 51):
+    
+    # Verifica se o número informado por `par` é par: 
+    if par % 2 == 0:
+
+        # Imprime na tela os números pares, e retira a quebra de linha:
+        print(par, end=' ')
