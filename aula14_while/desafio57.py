b10bebb8ac19e6818e63fbe44361b092eda640ff
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-08 19:44:03 -0300

    Resposta do desafio 57

--- aula14_while/desafio57.py
+++ aula14_while/desafio57.py
@@ -3,3 +3,18 @@
 Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'.
 Caso esteja errado, peça a digitação novamente até ter um valor correto.
 '''
+
+print('='*27)
+print('PEQUENA VALIDAÇÃO DE DADOS')
+print('='*27)
+
+sexo = input('Informe seu sexo [M/F]: ').upper()
+
+while sexo not in 'MF':
+    print('Resposta inválida.')
+    print('-='*20)
+    sexo = input('Informe seu sexo: [M/F]: ').upper()
+if sexo == 'M':
+    print('Você foi registrado com sucesso!')
+elif sexo == 'F':
+    print('Você foi registrada com sucesso!')
