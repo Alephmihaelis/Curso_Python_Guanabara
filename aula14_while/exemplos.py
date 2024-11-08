c466ab9e5e80b336226b430e823a4f38945cbc06
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-08 12:17:46 -0300

    Aula 14

---
+++ aula14_while/exemplos.py
@@ -0,0 +1,14 @@
+
+# Para a ocasião em que se sabe quantos números o usuário informará
+'''
+for c in range(1,5):
+    n = int(input('Digite um valor: '))
+print('Fim')
+
+
+# Para a ocasião em que não se sabe quantos números o usuário informará
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('Fim.')
+'''
