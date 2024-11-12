5cee95fd2c186f03dc12367954bd00f2814f8deb
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-12 17:51:27 -0300

    Update desafio61.py

--- aula14_while/desafio61.py
+++ aula14_while/desafio61.py
@@ -3,3 +3,15 @@
 Refaça o desafio 51, lendo o primeiro termo e a razão de uma P.A.,
 mostrando os 10 primeiros termos da progressão usando a estrutura `while`.
 '''
+
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+
+primeiro = termo
+contador = 1
+
+while contador <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    contador += 1
+print('Fim da Progressão Aritmética')
