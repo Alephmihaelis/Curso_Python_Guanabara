2c8e90a897f0b9d21cd384747e0611c76ac87f7f
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-12 17:43:51 -0300

    Respostas

--- aula13_for/desafio53.py
+++ aula13_for/desafio53.py
@@ -6,28 +6,18 @@
 
 # Código do professor:
 
-# Solicita ao usuário que digite uma frase, remove espaços do início e do fim,
-# e converte toda a frase para letras maiúsculas.
-frase = str(input('Digite uma frase: ')).strip().upper()
+frase = input('Digite uma frase: ').strip().upper()
 
-# Divide a frase em palavras, criando uma lista de palavras separadas por espaços.
 palavras = frase.split()
 
-# Junta todas as palavras da lista em uma única string, removendo os espaços.
 junto = ''.join(palavras)
 
-# Inicializa uma string vazia para armazenar a versão invertida da string 'junto'.
 inverso = ''
 
-# Loop que percorre a string 'junto' de trás para frente.
 for letra in range(len(junto)-1, -1, -1):
-    # Adiciona cada letra da string 'junto' à string 'inverso'.
     inverso += junto[letra]
 
-# Compara a string original 'junto' com a string invertida 'inverso'.
 if inverso == junto:
-    # Se forem iguais, imprime que a frase é um palíndromo.
     print('A frase digitada é um palíndromo!')
 else:
-    # Se não forem iguais, imprime que a frase não é um palíndromo.
     print('A frase digitada não é um palíndromo!')
--- aula14_while/desafio60.py
+++ aula14_while/desafio60.py
@@ -2,4 +2,50 @@
 '''
 Faça um programa que leia um número qualquer e mostre seu fatorial.
 Ex.: 5! = 5x4x3x2x1 = 120
+
+
+Há duas formas de calcular o fatorial de um número em Python: a primeira é com a biblioteca `math`; a outra, com o loop `while`.
+Primeiro método, com `math`:
+
+from math import factorial
+
+num = int(input('Digite um número para saber-lhe seu fatorial: '))
+print('O fatorial de {} vale {}'.format(num, factorial(num)))
+
+Segundo método, com `while`:
+
+
+n = int(input('Digite um número para saber-lhe seu fatorial: '))
+
+contador = n
+fatorial = 1
+
+print('O fatorial de {}! é'.format(n))
+
+while contador > 0:
+
+    print('{}'.format(contador), end='')
+
+    print(' x ' if contador > 1 else ' = ', end='')
+
+    fatorial *= contador
+    contador -= 1
+
+print('{}'.format(fatorial))
 '''
+
+# Cálculo de fatorial com `for`
+
+num = int(input('Digite um número: '))
+
+fatorial = 1
+contador = num
+
+print('{}! equivale a '.format(num), end='')
+
+for i in range(1, num + 1):
+    print('{}'.format(contador), end='')
+    fatorial *= i
+    contador -= 1
+    print(' x ' if contador > 0 else ' = ', end='')
+print(fatorial)
