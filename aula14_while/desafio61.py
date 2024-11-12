52950b020ca8f75923612debfef010a896ffec3f
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-12 18:15:10 -0300

    Atualização
    Observação: necessário revisão

--- aula14_while/desafio61.py
+++ aula14_while/desafio61.py
@@ -11,7 +11,7 @@ primeiro = termo
 contador = 1
 
 while contador <= 10:
-    print('{} -> '.format(termo), end='')
+    print('{} :: '.format(termo), end='')
     termo += razao
     contador += 1
 print('Fim da Progressão Aritmética')
--- aula14_while/desafio62.py
+++ aula14_while/desafio62.py
@@ -2,3 +2,31 @@
 '''
 Melhore o desafio 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra quando ele disser que quer mostrar 0 termos.
 '''
+
+print('Progressão aritmética'.upper())
+
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+
+primeiro = termo
+contador = 1
+total = 0
+mais = 10
+
+while mais != 0:
+
+    total += mais
+
+    while contador <= total:
+
+        print('{}'.format(termo), end=' :: ')
+
+        termo += razao
+
+        contador += 1
+
+    print('PAUSA')
+
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+
+print('Progressão finalizada com {} termos exibidos.'.format(total))
