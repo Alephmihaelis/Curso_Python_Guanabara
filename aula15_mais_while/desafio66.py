66703e16dfb55e19cbfa20914b3171e503a84cd5
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-15 14:28:30 -0300

    Update desafio66.py

--- aula15_mais_while/desafio66.py
+++ aula15_mais_while/desafio66.py
@@ -3,18 +3,18 @@
 Crie um programa que leia vários números inteiros pelo teclado. O programa só parará quando o usuário digitar "999", que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o "flag").
 '''
 
-nuns = soma = 0
+nums = soma = 0
 
 while True:
-    num = int(input('Digite um valor (999 para parar): '))
 
+    num = int(input('Digite um valor (999 para parar): '))
     if num == 999:
         break
     
-    nuns += 1
+    nums += 1
     soma += num
 
 print('~'*30)
-print(f'Você digitou {nuns} números.')
+print(f'Você digitou {nums} números.')
 print(f'A soma de todos os números vale {soma}')
 print('~'*30)
