fbb8982dc62f78aa891e00526b597807053690f1
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-15 15:34:43 -0300

    Update desafio69.py

--- aula15_mais_while/desafio69.py
+++ aula15_mais_while/desafio69.py
@@ -1,8 +1,53 @@
 
-'''
-Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer continuar ou não. No final, mostre:
+m_dezoito, homens, m_n_vinte = 0, 0, 0
 
-a) Quantas pessoas têm mais de 18 anos
-b) Quantos homens foram cadastrados
-c) Quantas mulheres têm menos de 20 anos
-'''
+print('~'*20)
+print('CADASTRO DE PESSOAS')
+print('~'*20)
+
+while True:
+    idade = int(input('Idade: '))
+    sexo = input('Sexo [M/F]: ').upper().strip()
+    print('-'*20)
+
+    if sexo not in 'MF':
+        print('Opção inválida. Tente novamente.')
+        sexo = input('Sexo [M/F]: ').upper().strip()
+    
+    if idade > 18:
+        m_dezoito += 1
+    
+    if sexo == 'M':
+        homens += 1
+    
+    if sexo == 'F':
+        if idade < 20:
+            m_n_vinte += 1
+
+    question = ' '
+    while question not in 'SN':
+        question = input('Deseja continuar? [S/N] ').upper().strip()
+    if question == 'N':
+        break
+    print('-'*20)
+
+if m_dezoito > 1:
+    print(f'{m_dezoito} pessoas têm mais de 18 anos.')
+elif m_dezoito == 1:
+    print('Somente uma das pessoas cadastradas têm mais de 18 anos.')
+elif m_dezoito == 0:
+    print('Nenhuma das pessoas cadastradas tem mais de 18 anos.')
+
+if homens > 1:
+    print(f'{homens} homens foram cadastrados.')
+elif homens == 1:
+    print('Somente um homem foi cadastrado.')
+elif homens == 0:
+    print('Nenhum homem foi cadastrado.')
+
+if m_n_vinte > 1:
+    print(f'{m_n_vinte} mulheres têm menos de 20 anos.')
+elif m_n_vinte == 1:
+    print('Somente uma das mulheres cadastradas tem mais de 20 anos.')
+elif m_n_vinte == 0:
+    print('Nenhuma mulher cadastrada tem menos de 20 anos.')
