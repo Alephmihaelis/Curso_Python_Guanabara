a80f5eea1be90087d0046d1f14bb98f943031017
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-15 13:49:39 -0300

    Resposta do desafio 67

--- aula15_mais_while/desafio67.py
+++ aula15_mais_while/desafio67.py
@@ -1,3 +1,16 @@
 
 '''
+Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 '''
+
+while True:
+    num = int(input('Você quer ver a tabuada de que valor? '))
+    print('~'*10)
+    for x in range(1, 10):
+        print(f'{num} x {x} = {num*x}')
+    print('~'*10)
+    response = int(input('Deseja continuar?\nSe sim, digite outro número.\nSe não, digite um número negativo. '))
+    if response < 0:
+        break
+
+print('Programa encerrado.')
