b2cebb07a1df94c85edc5a67137b8d116147fd13
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-15 16:26:02 -0300

    Update desafio70.py

--- aula15_mais_while/desafio70.py
+++ aula15_mais_while/desafio70.py
@@ -1,7 +1,29 @@
 
-'''
-Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário continuará. No final, mostre:
-a) Qual é o total gasto na compra
-b) Quantos produtos custam mais de R$1000
-c) Qual é o nome do produto mais barato
-'''
+total, mil, menor, cont = 0, 0, int(), 0
+
+barato = str()
+
+while True:
+    nome = input('Nome do produto: ')
+    preco = float(input('Preço: R$'))
+    cont += 1
+    total += preco
+
+    if preco > 1000:
+        mil += 1
+
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = nome
+
+    question = ' '
+    while question not in 'SN':
+        question = input('Quer continuar? [S/N] ').upper().strip()[0]
+    if question == 'N':
+        break
+
+print('~'*30)   
+print(f'Total da compra: R${total:.2f}')
+print(f'Número de protudos que custam mais de R$1000: {mil}')
+print(f'O nome do produto mais barato, {barato}, custa R${menor:.2f}.')
+print('~'*30)
