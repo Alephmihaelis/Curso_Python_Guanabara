87ae8289d01e2bea7854faba88a163c63b76ef4a
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-16 13:26:48 -0300

    Update desafio74.py

--- aula16_tuplas/desafio74.py
+++ aula16_tuplas/desafio74.py
@@ -3,3 +3,23 @@
 Crie um programa que gerará cinco números aleatórios e os colocará em uma tupla.
 Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
 '''
+
+from random import randint
+
+numeros = (randint(0, 10), randint(0, 10), randint(0, 10),
+           randint(0, 10), randint(0, 10))
+
+menor = maior = numeros[0]
+
+print('Os números gerados foram:', end = ' ')
+
+for n in numeros:
+    print(n, end = ' ')
+
+    if n > maior:
+        maior = n
+    if n < menor:
+        menor = n
+
+print(f'\nO menor número é {menor}')
+print(f'O maior número é {maior}')
