cc60c45488d1a827146d3dd22a29dcd8b02f0344
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-16 13:10:29 -0300

    Update desafio73.py

--- aula16_tuplas/desafio73.py
+++ aula16_tuplas/desafio73.py
@@ -1,8 +1,21 @@
 
-'''
-Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
-a) apenas os 5 primeiros colocados
-b) os últimos 4 colocados da tabela
-c) uma lista com os times em ordem alfabética
-d) em que posição na tabela está o time do Chapecoense
-'''
+times = ('Botafogo', 'Palmeiras', 'Fortaleza', 'Flamengo',
+             'Internacional', 'São Paulo', 'Cruzeiro', 'Bahia',
+             'Vasco', 'Atlético-MG', 'Corinthians', 'Grêmio',
+             'Vitória', 'Fluminense', 'Criciúma', 'Juventude',
+             'Bragantino', 'Athletico-PR', 'Cuiabá', 'Atlético-GO')
+
+print('~' * 30)
+print(f'Lista de times do Brasileirão: {times}')
+print('~' * 30)
+
+print(f'Os 5 primeiros colocados são: {times[0:5]}')
+print('~' * 30)
+
+print(f'Os últimos 4 colocados são: {times[-4:]}')
+print('~' * 30)
+
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('~' * 30)
+
+print(f'O Bahia está na {times.index("Bahia") + 1}ª posição.')
