0dbe2962d2d4b7980b946b66635a5c08dd21e422
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-16 14:21:52 -0300

    Update desafio76.py

--- aula16_tuplas/desafio76.py
+++ aula16_tuplas/desafio76.py
@@ -2,3 +2,28 @@
 '''
 Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 '''
+
+produtos = (input('Produto: '),
+            float(input('Preço: R$')),
+
+            input('Produto: '),
+            float(input('Preço: R$')),
+
+            input('Produto: '),
+            float(input('Preço: R$')),
+
+            input('Produto: '),
+            float(input('Preço: R$')),
+
+            input('Produto: '),
+            float(input('Preço: R$')),
+
+            input('Produto: '),
+            float(input('Preço: R$'))
+                  )
+
+for i in range (0, len(produtos)):
+    if i % 2 == 0:
+        print(f'{produtos[i]:.<30}', end='')
+    else:
+        print(f'R${produtos[i]:>7.2f}')
