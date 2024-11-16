902143704d5ee56b391e52ff4a9c0904aea3e79b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-16 12:41:38 -0300

    Update desafio72.py

--- aula16_tuplas/desafio72.py
+++ aula16_tuplas/desafio72.py
@@ -3,21 +3,23 @@ print('~' * 20)
 print('NÚMERO POR EXTENSO')
 print('~' * 20)
 
-num_tuple = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco',
-             'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
-             'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze',
-             'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-
-
-num = int(input('Digite um número de 0 a 20 para ver-lhe escrito por extenso: '))
+num_tuple = ('Zero', 'Um', 'Dois', 'Três', 'Quatro',
+             'Cinco', 'Seis', 'Sete', 'Oito', 'Nove',
+             'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze',
+             'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito',
+             'Dezenove', 'Vinte')
 
 while True:
-    if num > 20:
-        print('Opção inválida. Tente novamente.')
-        print('=' * 30)
-        num = int(input('Digite um número de 0 a 20 para ver-lhe escrito por extenso: '))
+
+    num = int(input('Digite um número de 0 a 20 para ver-lhe escrito por extenso: '))
+    if num >= 0 and num < 20:
+        print(f'O número {num}, escrito por extenso é "{num_tuple[num]}".')
+        
+        response = input('Quer continuar? [S/N] ').upper().strip()[0]
+        
+        if response == 'N':
+            print('Programa encerrado.')
+            break
     else:
-        break
-print('=' * 30)
-print(f'O número {num}, escrito por extenso é "{num_tuple[num]}".')
-print('=' * 30)
+        print('Opção inválida. Tente novamente.')
+        print('=-' * 30)
