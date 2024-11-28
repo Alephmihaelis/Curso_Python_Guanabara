de7b13b75dd7e8c4792b0b3535ff400ac690953c
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-28 14:02:22 -0300

    Update desafio83.py

--- aula17_lista_parte_1/desafio83.py
+++ aula17_lista_parte_1/desafio83.py
@@ -3,3 +3,24 @@
 Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 Seu aplicativo deverá analisar se a expressão digitada está com os parênteses abertos e fechados na ordem correta.
 '''
+
+expr = input('Expressão: ')
+pilha = []
+
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+
+print('~' * 30)
+
+if len(pilha) == 0:
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão é inválida!')
