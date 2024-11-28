1c4c9688d5f80749a6bf8c86716434a00973cbe6
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-28 12:17:38 -0300

    Update desafio80.py

--- aula17_lista_parte_1/desafio80.py
+++ aula17_lista_parte_1/desafio80.py
@@ -2,3 +2,18 @@
 '''
 Crie um programa onde o usuário possa digitar 5 valores números e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 '''
+
+numeros = []
+
+for p, num in enumerate(range(5)):
+    numero = int(input(f'Digite um valor na posição {p+1}: '))
+
+    if len(numeros) == 0 or numero > numeros[-1]:
+        numeros.append(numero)
+    else:
+        for i in range(len(numeros)):
+            if numero <= numeros[i]:
+                numeros.insert(i, numero)
+                break
+
+print(numeros)
