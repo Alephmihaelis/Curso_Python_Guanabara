7ed572bc316764fa879db359c01503f7f770b852
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-28 15:03:33 -0300

    Solução do desafio 84

--- aula18_lista_parte_2/desafio84.py
+++ aula18_lista_parte_2/desafio84.py
@@ -6,3 +6,39 @@ b) Uma listagem com as pessoas mais pesadas
 c) Uma listagem com as pessoas mais leves
 '''
 
+temp = []
+princ = []
+maior = menor = None
+
+while True:
+    temp.append(str(input('Nome: ')))
+    temp.append(float(input('Peso: ')))
+    
+    if len(princ) == 0:
+        maior = menor = temp[1]
+    else:
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    
+    princ.append(temp[:])
+    temp.clear()
+
+    response = input('Continuar? [S/N] ').upper().strip()
+
+    if response == 'N':
+        break
+
+print('~' * 40)
+
+print(f'Número de pessoas cadastradas: {len(princ)}')
+print(f'O maior peso foi de {maior}kg. Peso de: ', end='')
+for p in princ:
+    if p[1] == maior:
+        print(f'[{p[0]}]', end=', ')
+
+print(f'\nO menor peso foi de {menor}kg. Peso de: ', end='')
+for p in princ:
+        if p[1] == menor:
+            print(f'[{p[0]}]', end=', ')
