18e3eacf66c347d11e5fe5dfdc2c9958acc2787b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-28 12:26:34 -0300

    Update desafio81.py

--- aula17_lista_parte_1/desafio81.py
+++ aula17_lista_parte_1/desafio81.py
@@ -7,3 +7,27 @@ a) Quantos números foram digitados
 b) A lista de valores, ordenada de forma decrescente.
 c) Se o valor 5 foi digitado e está ou não na lista.
 '''
+
+numeros = []
+cont = 0
+
+cinco = False
+
+while True:
+    valor = int(input('Digite um valor: '))
+    numeros.append(valor)
+    cont += 1
+
+    if 5 in numeros:
+        cinco = True
+
+    response = input('Deseja continuar? [S/N] ').upper().strip()
+
+    if response == 'N':
+        break
+
+print('~' * 20)
+print(f'Você digitou {cont} números.')
+numeros.sort(reverse=True)
+print(f'A lista gerada por você, em ordem decrescente, é {numeros}')
+print(f'O valor 5 foi digitado? {"Sim" if cinco else "Não"}')
