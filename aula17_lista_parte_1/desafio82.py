74053d0d1fbd329e67c5e0eb86c64135fdf72e80
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-28 12:38:02 -0300

    Update desafio82.py

--- aula17_lista_parte_1/desafio82.py
+++ aula17_lista_parte_1/desafio82.py
@@ -4,3 +4,30 @@ Crie um programa que lerá vários números e os colocará numa lista.
 Depois, crie duas listas extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente.
 Ao final, mostre o conteúdo das três listas geradas.
 '''
+
+numeros = []
+pares = []
+impares = []
+
+while True:
+    valor = int(input('Digite um valor: '))
+
+    response = input('Deseja continuar? [S/N]: ').upper().strip()
+
+    if response not in ('S', 'N'):
+        print('Resposta inválida. Tente novamente.')
+        continue
+
+    numeros.append(valor)
+
+    if valor % 2 == 0:
+        pares.append(valor)
+    else:
+        impares.append(valor)
+
+    if response == 'N':
+        break
+
+print(f'Sua lista completa é: {numeros}')
+print(f'Sua lista só com os números pares é {pares}')
+print(f'Sua lista só com os números ímpares é {impares}')
