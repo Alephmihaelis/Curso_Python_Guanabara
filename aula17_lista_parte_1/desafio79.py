8cd9c29a7e3dad5aec7d4f8e5b5756d9e579c0b6
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-28 12:11:02 -0300

    Update desafio79.py

--- aula17_lista_parte_1/desafio79.py
+++ aula17_lista_parte_1/desafio79.py
@@ -1,4 +1,19 @@
 
 '''
-Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já existe lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados em ordem crescente.
+Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados em ordem crescente.
 '''
+
+numeros = []
+
+while True:
+    valor = int(input('Digite um valor: '))
+
+    if valor not in numeros:
+        numeros.append(valor)
+
+    response = input('Deseja continuar? [S/N]: ').upper().strip()
+    if response == 'N':
+        break
+
+numeros.sort()
+print(f'Você gerou a lista {numeros}')
