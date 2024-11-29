72894b77b7995a3402d0c744bd508dd33b06cb6c
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-29 16:09:02 -0300

    Update desafio88.py

--- aula18_lista_parte_2/desafio88.py
+++ aula18_lista_parte_2/desafio88.py
@@ -4,5 +4,35 @@ Faça um programa que ajude um jogador da MEGA SENA a criar palpites. O programa
 '''
 
 from random import randint
+from time import sleep
 
-nums = randint(1, 60)
+lista = []
+jogos = []
+
+print('~' * 30)
+print('PALPITES PARA A MEGA SENA')
+print('~' * 30)
+
+quant_jogos = int(input('Quantos jogos você quer fazer? '))
+
+tot_jogos = 1
+
+while tot_jogos <= quant_jogos:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot_jogos += 1
+
+print('-=' * 3, f'Sorteando {tot_jogos} jogos ', '-=' * 3)
+
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
