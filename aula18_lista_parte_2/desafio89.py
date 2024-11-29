dc53591339cd0f225f5517d3ad1fe87f53fc8b8b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-11-29 16:22:27 -0300

    Update desafio89.py

--- aula18_lista_parte_2/desafio89.py
+++ aula18_lista_parte_2/desafio89.py
@@ -1,4 +1,34 @@
 
 '''
-Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno indiviudalmente.
+Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta.
+No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno indiviudalmente.
 '''
+
+ficha = []
+
+while True:
+
+    nome = input('Nome: ')
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+
+    ficha.append([nome, [nota1, nota2], media])
+
+    resposta = input('Quer continuar? [S/N] ').upper().strip()
+    if resposta == 'N':
+        break
+
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('=' * 20)
+
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+
+while True:
+    print('-' * 25)
+    opc = int(input('Mostrar notas de que aluno? (999 interrompe): '))
+    if opc == 999:
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
