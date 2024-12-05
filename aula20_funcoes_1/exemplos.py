e7ae75726d02ab0d2fe627af2274c9dbebedcc9e
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-05 15:27:51 -0300

    Update exemplos.py

--- aula20_funcoes_1/exemplos.py
+++ aula20_funcoes_1/exemplos.py
@@ -8,13 +8,11 @@ Funções têm por objetivo o fazer constantemente coisas que serão constanteme
 def lin():
     print('—' * 30)
 
-lin()
-print('SISTEMA DE ALUNOS')
-lin()
-
 def mensagem(msg):
     print('—' * 30)
     print(msg)
     print('—' * 30)
 
-mensagem('APRENDENDO FUNÇÕES')
+mensagem('APRENDENDO FUNÇÕES!')
+mensagem('Python é muito legal!')
+mensagem('I heard you struck my son...')
