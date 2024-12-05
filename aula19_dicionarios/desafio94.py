1ed0edd060d5ffeaa380e830a8dbb992ec168e42
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-05 14:51:21 -0300

    Update desafio94.py

--- aula19_dicionarios/desafio94.py
+++ aula19_dicionarios/desafio94.py
@@ -7,3 +7,43 @@ b) A média de idade do grupo;
 c) Uma lista com todas as mulheres;
 d) Uma lista com todas as pessoas com idade acima da média.
 '''
+
+galera = []
+pessoa = {}
+soma = media = 0
+
+while True:
+    pessoa.clear()
+    pessoa['nome'] = input('Nome: ')
+    while True:
+        pessoa['sexo'] = input('Sexo: [M/F] ').upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Dado inválido.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = input('Deseja continuar? [S/N] ').upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO. Dado inválido.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'Total de pessoas cadastradas: {len(galera)}')
+print(f'Média de idade: {soma / len(galera):.2f}')
+print(f'As mulheres cadastradas foram ', end='')
+
+for p in galera:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]} ', end='')
+
+print('\nLista das pessoas que estão acima da média de idade do grupo: ', end='')
+for p in galera:
+    if p['idade'] >= soma / len(galera):
+        print('        ', end='')
+        for k, v in p.items():
+            print(f'{k}, {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
