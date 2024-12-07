fe09a7d3df3faa2a4ca97fb952d3ec98dd0f5cef
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-07 15:44:29 -0300

    Update desafio96.py

--- aula20_funcoes_1/desafio96.py
+++ aula20_funcoes_1/desafio96.py
@@ -3,12 +3,15 @@
 Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno (lagura e comprimento) e mostre a área do terreno.
 '''
 
-def calcula_area():
-    larg = float(input('Largura do terreno (m): '))
-    alt = float(input('Altura do terreno (m): '))
-    print(f'A área de um terreno {larg}x{alt} vale {larg * alt}m² ')
+def calcula_area(larg, comp):
+    print(f'A área de um terreno {larg}x{comp} vale {l * c}m² ')
+
 
 print('~' * 30)
 print('Calculando área de um terreno')
 print('~' * 30)
-calcula_area()
+
+l = float(input('Largura do terreno (m): '))
+c = float(input('Comprimento do terreno (m): '))
+
+calcula_area(l, c)
