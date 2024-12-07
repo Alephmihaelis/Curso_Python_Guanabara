75931df25660254531ed1181ecbe01da772e27e8
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-07 16:33:57 -0300

    Update desafio99.py

--- aula20_funcoes_1/desafio99.py
+++ aula20_funcoes_1/desafio99.py
@@ -1,4 +1,29 @@
 
 '''
-Faça um programa que tenha uma função maior(), que receba várias parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+Faça um programa que tenha uma função maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 '''
+
+from time import sleep
+
+def maior(* n):
+    cont = maior = 0
+    print('-=' * 30)
+    print('Analisando os valores informados...')
+    for valor in n:
+        print(f'{valor}', end=' ', flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont +=1
+    print(f'Foram informados {cont} valores ao todo.')
+    print(f'O maior valor informado foi {maior}')
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
