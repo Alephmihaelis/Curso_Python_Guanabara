48b348ae272ad858908dde3cb6b396efc7600e8d
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-07 17:11:03 -0300

    Update exemplo.py

--- aula21_funcoes_2/exemplo.py
+++ aula21_funcoes_2/exemplo.py
@@ -35,4 +35,11 @@ def contador(i, f, p): ### Parâmetro real
 contador(2, 10, 2) ### Parâmetro formal
 help(contador)
 
+
+##### Parâmetro opcional #####
+def somar(a=0, b=0, c=0):
+    s = a + b + c
+    print(f'A soma vale {s}')
 somar(3, 2, 5)
+somar(8, 4)
+somar()
