f040fdab10a67ca88b29711e2914161241934be8
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-07 16:24:24 -0300

    Update desafio98.py

--- aula20_funcoes_1/desafio98.py
+++ aula20_funcoes_1/desafio98.py
@@ -5,3 +5,42 @@ a) De 1 até 10, de 1 em 1
 b) De 10 até 0, de 2 em 2
 c) Uma contagem personalizada
 '''
+
+from time import sleep
+
+def contador(i, f, p):
+
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont += p
+        print('Terminou!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ',end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('Terminou!')
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('-=' * 20)
+
+print('Contagem personalizada!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+
+contador(inicio, fim, passo)
