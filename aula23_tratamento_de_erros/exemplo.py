b7836ff4ded96f19731a1d92e588ced2532e2d9b
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-10 16:39:43 -0300

    Update exemplo.py

--- aula23_tratamento_de_erros/exemplo.py
+++ aula23_tratamento_de_erros/exemplo.py
@@ -3,3 +3,9 @@
 Erros e exceções.
 '''
 
+primt(x) # Erro de sintaxe.
+
+# O erro sintático é só um tipo de erro. HÁ OUTROS ERROS.
+
+print(x) # Não há erro sintático, mas há erro, porque a variável `x` não foi inicializada. Isso é um ERRO SEMÂNTICO. Neste caso, o `x` retorna uma EXCEÇÃO `NameError`
+
