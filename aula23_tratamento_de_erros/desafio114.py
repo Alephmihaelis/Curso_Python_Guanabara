9cf1a4620fb42f62f9b85217d41674bc62790775
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-10 17:25:39 -0300

    Update desafio114.py

--- aula23_tratamento_de_erros/desafio114.py
+++ aula23_tratamento_de_erros/desafio114.py
@@ -1,4 +1,16 @@
 
 '''
-Crie um código em Python que teste se o site Pudim está acessível no computador usado.
+Crie um código em Python que teste se um site está acessível no computador usado.
 '''
+
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('https://www.youtube.com')
+
+except urllib.error.URLError:
+    print('O site não está acessível no momento.')
+
+else:
+    print('Tudo certo! ;D')
