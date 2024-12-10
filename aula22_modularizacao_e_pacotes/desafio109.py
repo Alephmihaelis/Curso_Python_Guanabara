99bc8e854ec8854d0ce1db228930240face9c240
Alephmihaelis <lyraportvgvesa@gmail.com>
2024-12-10 15:02:01 -0300

    att

--- aula22_modularizacao_e_pacotes/desafio109.py
+++ aula22_modularizacao_e_pacotes/desafio109.py
@@ -1,3 +1,27 @@
 
 '''
+Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(), desenvolvida no desafio 108.
+'''
+
+from moeda import diminuir
+
+num = float(input('Digite um valor em reais: '))
+valor = float(input('Quanto você quer subtrair? '))
+print('Valor total: ', end='')
+print(diminuir(num, valor, True))
+
+'''
+Não fiz este desafio, porque a solução que gerei para os meus módulos já "matava" o problema apontado pelo professor.
+
+Por outro lado, eu poderia escrever a função que formata a saída de dados da seguinte maneira:
+def moedas(valor, fmt=False):
+    n_format = f'R${valor}'
+    if format:
+        res = f'R${valor:.2f}'.replace(".", ",")
+        return res
+    else:
+        return n_format
+
+No fim, essa ficou sendo a função final.
+        
 '''
--- aula22_modularizacao_e_pacotes/moeda.py
+++ aula22_modularizacao_e_pacotes/moeda.py
@@ -1,26 +1,31 @@
 
-def moedas(valor):
-    res = f'R${valor:.2f}'.replace(".", ",")
-    return res
+
+def moedas(valor, fmt=False):
+    n_format = f'R${valor}'
+    if fmt:
+        res = f'R${valor:.2f}'.replace(".", ",")
+        return res
+    else:
+        return n_format
 
 
-def aumentar(n = 0, valor=0):
+def aumentar(n = 0, valor=0, fmt=False):
     soma = n + valor
-    return moedas(soma)
+    return moedas(soma, fmt)
     
     
-def diminuir(valor = 0, n=0):
+def diminuir(valor = 0, n=0, fmt=False):
     subtracao = valor - n
-    return moedas(subtracao)
+    return moedas(subtracao, fmt)
     
     
-def dobro(valor = 0):
+def dobro(valor = 0, fmt=False):
     dobro = valor * 2
-    return moedas(dobro)
+    return moedas(dobro, fmt)
     
     
-def metade(valor = 0):
+def metade(valor = 0, fmt=False):
     metade = valor / 2
-    return moedas(metade)
+    return moedas(metade, fmt)
 
 
