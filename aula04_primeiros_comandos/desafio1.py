594541128f1756242b619812a2d69cb566055805
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 09:37:48 -0300

    Update desafio1.py

--- aula04_primeiros_comandos/desafio1.py
+++ aula04_primeiros_comandos/desafio1.py
@@ -1,9 +1,8 @@
+
 print('-=-'*15)
 print('\033[1;33;45mOlá, mundo! Estou aprendendo Python!\033[m')
 print('-=-'*15)
 
-nome = input('\033[4;32mQual seu nome?\033[m ')
+print('\033[4;32mQual seu nome?\033[m')
+nome = input().strip()
 print('Olá, ' + nome + '! É um prazer te conhecer!')
-
-nome2 = input('\033[1mQual seu nome?\033[m ')
-print('Olá, \033[1;32m{}\033[m! É um prazer conhecer você. :D'.format(nome2))
