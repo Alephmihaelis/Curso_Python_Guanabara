41a77015f11754604cb3f1cc64d3958dbcb4ef56
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 09:45:10 -0300

    Update desafio3_com_tipo_primitivo.py
    Melhorias na estruturação do código

--- aula06_tipos_primitivos_e_saida_de_dados/desafio3_com_tipo_primitivo.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio3_com_tipo_primitivo.py
@@ -1,9 +1,9 @@
 
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-s = n1 + n2
-print('\033[7;32;41mA soma entre {} e {} vale {}\033[m'.format(
-    n1,
-    n2,
-    s
-    ))
+print('Primeiro número:')
+n1 = int(input())
+
+print('Segundo número:')
+n2 = int(input())
+
+soma = n1 + n2
+print('\033[7;32;41mA soma entre {} e {} vale {}\033[m'.format(n1, n2, soma))
