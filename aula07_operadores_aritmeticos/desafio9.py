ddf547021e916007d7f7daf8fcb23074c7f191d1
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 13:49:26 -0300

    Update desafio9.py

--- aula07_operadores_aritmeticos/desafio9.py
+++ aula07_operadores_aritmeticos/desafio9.py
@@ -7,10 +7,10 @@ print('Digite um número para ver-lhe sua tabuada.')
 
 num = int(input())
 
-print('='*25)
+print('=' * 25)
 print(f'TABUADA DE {num}')
 
 for i in range(1, 11):
     print(f'{num} x {i} = {num * i}', sep='\t')
 
-print('='*25)
+print('=' * 25)
