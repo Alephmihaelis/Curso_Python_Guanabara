6eb9481e2f3387fbc493ca4dd6d7b390c12d1616
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 13:45:03 -0300

    Update desafio8.py
    Melhorias na legibilidade do código

--- aula07_operadores_aritmeticos/desafio8.py
+++ aula07_operadores_aritmeticos/desafio8.py
@@ -1,20 +1,17 @@
 
-# Escreva um programa que leia um valor em metros e o exiba convertido em outras unidades de comprimento.
+"""
+Programa que lê um valor em metros e o exibe convertido em outras unidades de comprimento.
+"""
 
-alpha = float(input('Digite um valor em metros: '))
-print('Analisando o valor informado, percebe-se que:\n\033[31m{}m\033[m equivale(m) a \033[32m{}km\033[m\n\033[33m{}m\033[m equivale(m) a \033[34m{}hm\033[m\n\033[35m{}m\033[m equivale(m) a \033[36m{}dam\033[m\n{}m equivale(m) a \033[37m{:.0f}dm\033[m\n{}m equivale(m) a {:.0f}cm\n{}m equivale(m) a {:.0f}mm'.format(
-    alpha,
-    (alpha/1000),
-    alpha,
-    (alpha/100),
-    alpha,
-    (alpha/10),
-    alpha,
-    (alpha*10),
-    alpha,
-    (alpha*100),
-    alpha,
-    (alpha*1000)
-    ))
+print('Digite um valor.')
+valor = float(input())
 
-# Ao depois procurarei maneiras de tornar este código mais legível.
+print('=' * 30)
+print('VALOR ORIGINAL | VALOR CONVERTIDO', sep='\t')
+print(f'''{valor}m equivale(m) a {valor/1000}km
+{valor}m equivale(m) a {valor/100}hm
+{valor}m equivale(m) a {valor/10}dam
+{valor}m equivale(m) a {valor*10:.0f}dm
+{valor}m equivale(m) a {valor*100:.0f}cm
+{valor}m equivale(m) a {valor*1000:.0f}mm''', sep='\t')
+print('=' * 30)
