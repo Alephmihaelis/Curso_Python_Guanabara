109f49e2331bf199d353e7e2d06660c9a33b2bab
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 13:53:34 -0300

    Update desafio10.py
    Melhorias na legibilidade do código. Valores de quotação (em parte) fictícios.

--- aula07_operadores_aritmeticos/desafio10.py
+++ aula07_operadores_aritmeticos/desafio10.py
@@ -1,13 +1,16 @@
 
-# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere US$1.00 = R$3.27
+"""
+Programa que lê quanto dinheiro uma pessoa tem na carteira,
+e mostra quantos dólares ela pode comprar. Considere US$1.00 = R$3.27
+OBS.: Em vez de R$3.27, pus a real cotação do dólar, que hoje é R$5.69
+OBS².: Adicionei euro e iene.
+"""
 
-# OBS.: Em vez de R$3.27, pus a real cotação do dólar, que hoje é R$5.69
-# OBS².: Adicionei euro e iene.
+print('Quantos reais você tem? R$')
 
-alpha = float(input('Quanto dinheiro você tem? R$'))
-print('\033[7;32;45mCom R${:.2f}, você pode comprar:\nUS${:.2f}\n€{:.2f}\n¥{:.2f}\033[m'.format(
-    alpha,
-    (alpha / 5.69),
-    (alpha / 6.19),
-    (alpha / 0.038)
-    ))
+reais = float(input())
+
+print(f'''Com R${reais:.2f}, você pode comprar:
+US${reais / 5.69:.2f}
+€{reais / 6.19:.2f}
+¥{reais / 0.038:.2f}''')
