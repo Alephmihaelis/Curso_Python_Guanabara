c1d6eec3b290b4eba412a27757473299bc7ffc7b
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 09:51:12 -0300

    Update desafio4.py
    Pequena melhorias na legibilidade do código.

--- aula06_tipos_primitivos_e_saida_de_dados/desafio4.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio4.py
@@ -1,58 +1,42 @@
 
-# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+"""
+Faça um programa que leia algo pelo teclado
+e mostre na tela o seu tipo primitivo
+e todas as informações possíveis sobre ele.
+"""
 
-object = input('Digite alguma coisa: ')
+print('Digite alguma coisa.')
+qualquerCoisa = input()
+
 print('\033[7;36;43mO tipo primitivo de "{}" é {}\033[m'.format(
-    object,
-    type(object)
-    ))
+    qualquerCoisa, type(qualquerCoisa)))
 
 print('\033[1;32;43m"{}" só tem espaços? {}\033[m'.format(
-    object,
-    object.isspace()
-    ))
+    qualquerCoisa, qualquerCoisa.isspace()))
 
 print('\033[4;33;44m"{}" pode ser um número? {}\033[m'.format(
-    object,
-    object.isnumeric()
-    ))
+    qualquerCoisa, qualquerCoisa.isnumeric()))
 
 print('\033[1;34;45m"{}" é alfabético? {}\033[m'.format(
-    object,
-    object.isalpha()
-    ))
+    qualquerCoisa, qualquerCoisa.isalpha()))
 
 print('\033[32;47m"{}" é alfanumérico? {}\033[m'.format(
-    object,
-    object.isalnum()
-    ))
+    qualquerCoisa, qualquerCoisa.isalnum()))
 
 print('\033[36;46m"{}" só tem letras maiúsculas? {}\033[m'.format(
-    object,
-    object.isupper()
-    ))
+    qualquerCoisa, qualquerCoisa.isupper()))
 
 print('\033[1;31;41m"{}" só tem letras minúsculas? {}\033[m'.format(
-    object,
-    object.islower()
-    ))
+    qualquerCoisa, qualquerCoisa.islower()))
 
 print('\033[4;32;43m"{}" está capitalizado? {}\033[m'.format(
-    object,
-    object.istitle()
-    ))
+    qualquerCoisa, qualquerCoisa.istitle()))
 
 print('\033[1;33;44mEm "{}", todos os caracteres são ASCII? {}\033[m'.format(
-    object,
-    object.isascii()
-    ))
+    qualquerCoisa, qualquerCoisa.isascii()))
 
 print('\033[7;31;45mEm "{}", todos os caracteres são decimais? {}\033[m'.format(
-    object,
-    object.isdecimal()
-    ))
+    qualquerCoisa, qualquerCoisa.isdecimal()))
 
 print('\033[1;34;44m"{}" é um identificador válido? {}\033[m'.format(
-    object,
-    object.isidentifier()
-    ))
+    qualquerCoisa, qualquerCoisa.isidentifier()))
