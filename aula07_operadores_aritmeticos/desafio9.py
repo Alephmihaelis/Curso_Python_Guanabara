1863c58246bd1821601705cbf81e4c6143545562
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 13:48:13 -0300

    Update desafio9.py
    Melhorias na lógica do código e na sua legibilidade

--- aula07_operadores_aritmeticos/desafio9.py
+++ aula07_operadores_aritmeticos/desafio9.py
@@ -1,31 +1,16 @@
 
-# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+"""
+Programa que lê um número inteiro qualquer e mostra na tela sua tabuada.
+"""
 
-alpha = int(input('Digite um número para ver-lhe a tabuada: '))
+print('Digite um número para ver-lhe sua tabuada.')
 
-print('-'*25)
+num = int(input())
 
-print('{} x 1 = \033[31m{}\033[m\n{} x 2 = \033[31m{}\033[m\n{} x 3 = \033[31m{}\033[m\n{} x 4 = \033[31m{}\033[m\n{} x 5 = \033[31m{}\033[m\n{} x 6 = \033[31m{}\033[m\n{} x 7 = \033[31m{}\033[m\n{} x 8 = \033[31m{}\033[m\n{} x 9 = \033[31m{}\033[m\n{} x 10 = \033[31m{}\033[m'.format(
-    alpha,
-    (alpha*1),
-    alpha,
-    (alpha*2),
-    alpha,
-    (alpha*3),
-    alpha,
-    (alpha*4),
-    alpha,
-    (alpha*5),
-    alpha,
-    (alpha*6),
-    alpha,
-    (alpha*7),
-    alpha,
-    (alpha*8),
-    alpha,
-    (alpha*9),
-    alpha,
-    (alpha*10)
-    ))
+print('='*25)
+print(f'TABUADA DE {num}')
 
-print('-'*25)
+for i in range(1, 11):
+    print(f'{num} x {i} = {num * i}', sep='\t')
+
+print('='*25)
