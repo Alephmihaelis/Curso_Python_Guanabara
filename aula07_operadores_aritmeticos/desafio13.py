ce60600168c9701a8257966d042b511a529f2bc0
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 18:26:14 -0300

    Update desafio13.py

--- aula07_operadores_aritmeticos/desafio13.py
+++ aula07_operadores_aritmeticos/desafio13.py
@@ -1,5 +1,8 @@
 
-# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+"""
+Algoritmo que lê o salário de um funcionário
+e mostra seu novo salário, com 15% de aumento.
+"""
 
 colors = {
     'green': '\033[32m',
@@ -7,15 +10,13 @@ colors = {
     'limpa': '\033[m'
 }
 
-salario = float(input('Salário do funcionário: R$'))
+print('Salário do funcionário: R$')
+salario = float(input())
 
-print('{}{}Com 15% de aumento, o salário do funcionário, que é de R${:.2f}, passará a ser \nR${:.2f}{}\n"AAAAAAAAAAAAAAAA"'.format(
+print('{}{}Com 15% de aumento, o salário do funcionário, que é de R${:.2f}, passará a ser\nR${:.2f}{}'.format(
     colors['green'],
     colors['under'],
     salario,
     salario + (salario * 15/100),
     colors['limpa']
     ))
-
-# Continuo evitando o uso desnecessário de variáveis, uma vez que o resultado é exibido uma vez só.
-# Este `"AAAAAAAAAAAAAAAA"` foi um teste; aparentemente, basta fechar o comando uma só vez.
