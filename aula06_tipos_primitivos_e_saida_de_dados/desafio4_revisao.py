7813ccf85b184e63054af26c0648e67ac5b0e86a
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 09:52:40 -0300

    Update desafio4_revisao.py
    Pequenas melhorias na legibilidade do código. Trata-se apenas de uma revisão do exercício 4.

--- aula06_tipos_primitivos_e_saida_de_dados/desafio4_revisao.py
+++ aula06_tipos_primitivos_e_saida_de_dados/desafio4_revisao.py
@@ -1,52 +1,33 @@
 
-something = input('Digite alguma coisa: ')
+print('Digite algo.')
+something = input()
 
 print('\033[4mO tipo primitivo de "{}" é {}\033[m'.format(
-    something,
-    type(something)
-    ))
+    something, type(something)))
 
 print('\033[1m"{}" é alfabético? {}\033[m'.format(
-    something,
-    something.isalpha()
-    ))
+    something, something.isalpha()))
 
 print('\033[0;37;47m"{}" é alfanumérico? {}\033[m'.format(
-    something,
-    something.isalnum()
-    ))
+    something, something.isalnum()))
 
 print('\033[7;36;36m"{}" pode ser um número? {}\033[m'.format(
-    something,
-    something.isnumeric()
-    ))
+    something, something.isnumeric()))
 
 print('\033[1;35;45m"{}" é um número? {}\033[m'.format(
-    something,
-    something.isdigit()
-    ))
+    something, something.isdigit()))
 
 print('\033[1;34;44m"{}" está capitalizado? {}\033[m'.format(
-    something,
-    something.istitle()
-    ))
+    something, something.istitle()))
 
 print('\033[4;33;43m"{}" está de todo em letras maiúsculas? {}\033[m'.format(
-    something,
-    something.isupper()
-    ))
+    something, something.isupper()))
 
 print('\033[1;32;42m"{}" está de todo em letras minúsculas? {}\033[m'.format(
-    something,
-    something.islower()
-    ))
+    something, something.islower()))
 
 print('"{}" pode ser impresso? {}'.format(
-    something,
-    something.isprintable()
-    ))
+    something, something.isprintable()))
 
 print('"{}" compõe-se só de espaços? {}'.format(
-    something,
-    something.isspace()
-    ))
+    something, something.isspace()))
