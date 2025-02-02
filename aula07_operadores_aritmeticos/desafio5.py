f1cda4b9d2b8984dba8812c8d704984e40afaaa1
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-02 10:30:47 -0300

    Update desafio5.py
    Melhorias na lógica do programa; adição de uma funcionalidade que contabiliza o número de tentativas. Melhorias na estruturação do programa

--- aula07_operadores_aritmeticos/desafio5.py
+++ aula07_operadores_aritmeticos/desafio5.py
@@ -2,6 +2,9 @@
 """
 Programa que lê um número inteiro e mostra na tela seu sucessor e antecessor.
 """
+from sys import exit
+
+tentativas = 0
 
 MSG = 'ANTECESSOR E SUCESSOR'
 print('=' * len(MSG))
@@ -18,8 +21,13 @@ Sucessor de \033[1m"{}"\033[m: \033[1m"{}"\033[m.""".format(
 num, (num - 1), num, (num + 1)))
         break
     except ValueError:
+        tentativas += 1
+        if tentativas == 3:
+            print('Número máximo de tentativas atingido. PROGRAMA ENCERRADO.')
+            exit()
         print('Digite um valor válido.')
         continue
     except Exception as e:
         print(f'Ocorreu um erro inesperado: {e}')
         continue
+print('=' * 25)
