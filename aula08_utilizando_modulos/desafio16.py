d432f8df39c3d0036bf51367bd3a9bf85aee7e37
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-03 12:53:10 -0300

    Update desafio16.py
    Melhoria na legibilidade do código

--- aula08_utilizando_modulos/desafio16.py
+++ aula08_utilizando_modulos/desafio16.py
@@ -1,26 +1,25 @@
 
 from math import trunc, floor
 
-# Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira. Ex: Digite um número: 6.127 :: O número 6.127 tem a parte inteira 6.
+'''
+Programa que lê um número real qualquer pelo teclado e mostra na tela a sua
+porção inteira. Ex.: Digite um número: 6.127 -> O número 6.127 tem a parte inteira 6.
+'''
 
-num_real = float(input('Digite um número real: '))
+print('Digite um número real.')
+numReal = float(input())
 
-# Há três modos de resolver este exercício. Posso resolvê-lo ou com a função `int`, ou com o método `trunc`, ou com o método `floor`.
+'''
+Há três modos de resolver este exercício.
+Posso resolvê-lo ou com a função `int`, ou com o método `trunc`,
+ou com o método `floor`.
+'''
 
 # Resolução com a função `int`
-print('A porção inteira de {} é {}'.format(
-    num_real,
-    int(num_real)
-    ))
+print('A porção inteira de {} é {}'.format(numReal, int(numReal)))
 
 # Resolução com o método `trunc`
-print('A porção inteira de {} é {}'.format(
-    num_real,
-    trunc(num_real)
-    ))
+print('A porção inteira de {} é {}'.format(numReal, trunc(numReal)))
 
 # Resolução com o método `floor`
-print('A porção inteira de {} é {}'.format(
-    num_real,
-    floor(num_real)
-    ))
+print('A porção inteira de {} é {}'.format(numReal, floor(numReal)))
