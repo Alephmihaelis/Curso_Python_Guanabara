5619116215d59ef0b9401373baf750cffb696c1d
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-03 11:25:02 -0300

    Update desafio14.py

--- aula07_operadores_aritmeticos/desafio14.py
+++ aula07_operadores_aritmeticos/desafio14.py
@@ -1,29 +1,37 @@
 
-# Converta uma temperatura digitada em Celsius para Fahrenheit.
-# OBS.: Adicionei também o código que faz a conversão de Fahrenheit para Celsius.
+"""
+Converte uma temperatura digitada em Celsius para Fahrenheit.
+OBS.: Adicionei também o código que faz a conversão de Fahrenheit para Celsius.
+"""
 
-colors = {
-    'red_bg': '\033[41m',
-    'blue_bg': '\033[44m',
-    'bold': '\033[1m',
-    'limpa': '\033[m'
-}
+MSG = 'CONVERSOR DE TEMPERATURAS'
 
-print('Temperatura de Celsius para Fahrenheit')
-temp_c = float(input('Temperatura em ºC: '))
-print('{}A temperatura {:.1f}ºC, em Fahrenheit, vale {:.1f}ºF{}'.format(
-    colors['red_bg'],
-    temp_c, temp_c * 1.8 + 32,
-    colors['limpa']
-    ))
+print('=' * len(MSG))
+print(MSG)
+print('=' * len(MSG))
 
-print('-'*40)
+print('''Convertemos Celsius em Fahrenheit e vice-versa.
+[1] para converter Celsius para Fahrenheit
+[2] para converter Fahrenheit para Celsius''')
 
-print('Temperatura de Fahrenheit para Celsius')
-temp_f = float(input('Temperatura em ºF: '))
-print('{}A temperatura {:.1f}ºF equivale a {:.1f}ºC{}'.format(
-    colors['blue_bg'],
-    temp_f,
-    (temp_f - 32) / 1.8,
-    colors['limpa']
-    ))
+while True:
+    try:
+        escolha = int(input())
+
+        if escolha == 1:
+            print('Temperatura em ºC:')
+            temp_c = float(input())
+            print('A temperatura {:.1f}ºC, em Fahrenheit, vale {:.1f}ºF'.format(temp_c, temp_c * 1.8 + 32,))
+            break
+        elif escolha == 2:
+            print('Temperatura em ºF:')
+            temp_f = float(input())
+            print('A temperatura {:.1f}ºF equivale a {:.1f}ºC'.format(temp_f, (temp_f - 32) / 1.8))
+            break
+        else:
+            print('Insira um número válido.')
+            continue
+
+    except ValueError:
+        print('Insira um número válido.')
+        continue
