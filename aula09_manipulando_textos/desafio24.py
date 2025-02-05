a0b4d27d56d1ebda99784d17c5f7b69676cb7860
Alephmihaelis <lyraportvgvesa@gmail.com>
2025-02-05 11:24:38 -0300

    Update desafio24.py

--- aula09_manipulando_textos/desafio24.py
+++ aula09_manipulando_textos/desafio24.py
@@ -1,12 +1,28 @@
 
-'''
-Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavra 'Santo'
-'''
+"""
+Programa que lê o nome de uma cidade
+e diz se ele começa ou não com a palavra «Santo»
+"""
 
-city_name = input('Nome da cidade: ').strip().title()
-print('A cidade digitada foi "{}".\nEla começa com "Santo"? {}'.format(
-    city_name,
-    'Santo' in city_name.split()[0]
-))
+"""
+print('Qual o nome da cidade?')
+nomeCidade = input().strip().title()
+print('''A cidade digitada foi "{}".
+Ela começa com "Santo"? {}'''.format(nomeCidade, 'Santo' in nomeCidade.split()[0]))
+"""
 
-# Meu código foi um pouco diferente do do professor. Usei o operador `in`.
+# Resolvendo o problema com `regex`
+
+import re
+
+def validaNomeCidade(cidade):
+    cidadeRegex = re.compile(r'^santo', re.IGNORECASE)
+    mo = cidadeRegex.search(cidade)
+    if mo:
+        print('A cidade digitada começa com «Santo»')
+    else:
+        print('Essa cidade não começa com «Santo»')
+
+print('Informe o nome da cidade.')
+nomeCidade = input()
+validaNomeCidade(nomeCidade)
